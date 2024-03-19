a6ace55f2deb019cbc9d6f2593df09c544454339
MASISOMETAL <cristianmasisofarias@gmail.com>
2024-03-18 22:37:23 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input} />
+                <Button title="ADD" color="#5555ff" />
+            </View>
+
+            <View style={styles.taskContainer}>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Item 1</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Item 2</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Item 3</Text>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.taskText}>Item 4</Text>
+                </View>
+            </View>
+        </View>
+    )
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        paddingTop: 30,
+        alignItems: "center",
+        backgroundColor: "#888888",
+        flex: 1
+    },
+    inputContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 20,
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderBottomColor: "black",
+        width: 250,
+        fontSize: 16
+    },
+    taskContainer: {
+        borderWidth: 1,
+        marginTop: 15,
+        alignItems: "center",
+        width: "90%",
+        paddingVertical: 10
+    },
+    card: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#cccccc",
+        width: "90%",
+        paddingVertical: 15,
+        marginVertical: 10,
+        borderRadius: 5
+    },
+    taskText: {
+        fontWeight: "bold",
+        fontSize: 16
+    },
+})
